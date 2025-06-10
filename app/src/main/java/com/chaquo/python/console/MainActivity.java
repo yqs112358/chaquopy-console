94a3fca5c8156b5f6333e42416190b2b02108eef
YQ <yqs112358@gmail.com>
2025-06-10 16:30:43 +0800

    try to add libadb

--- app/src/main/java/com/chaquo/python/console/MainActivity.java
+++ app/src/main/java/com/chaquo/python/console/MainActivity.java
@@ -1,7 +1,16 @@
 package com.chaquo.python.console;
 
 import android.app.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import com.chaquo.python.utils.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends PythonConsoleActivity {
 
@@ -21,7 +30,33 @@ public class MainActivity extends PythonConsoleActivity {
         }
 
         @Override public void run() {
+
             py.getModule("main").callAttr("main");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String binaryName = "libadb.so";
+        String nativeLibsDir = getApplicationInfo().nativeLibraryDir;
+        String binaryPath = nativeLibsDir + File.separator + binaryName;
+        String cmd = binaryPath + " --version";
+
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            InputStream inputStream = process.getInputStream();
+            int c;
+            StringBuilder output = new StringBuilder();
+            while ((c = inputStream.read()) != -1) {
+                output.append((char) c);
+            }
+            inputStream.close();
+            process.waitFor();
+            Toast.makeText(this, output.toString(), Toast.LENGTH_LONG).show();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
